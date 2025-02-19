7833c284a9bbe08d5a3f1d6e4d5f4c2e3783b396
Naim <naimadanileiva@gmail.com>
2025-02-19 13:38:04 -0300

    Firebase implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Reel from './components/Reel';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
-import Navbar from './components/Navbar';
+import Cart from './components/Cart';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Cart from './components/Cart';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0Sf88vsTZx6kcJLPOV4hTevu5mPn-pcE",
+  authDomain: "raviolandia-bb645.firebaseapp.com",
+  projectId: "raviolandia-bb645",
+  storageBucket: "raviolandia-bb645.firebasestorage.app",
+  messagingSenderId: "717057749513",
+  appId: "1:717057749513:web:43f21f1751316e8984bed8",
+  measurementId: "G-VVBY18FSLM"
+};
+
+const App = initializeApp(firebaseConfig);
+const analytics = getAnalytics(App);
 
 function App() {
   return (
